a1388b1e2e984cff45feb86d5b26d4915cbc900f
Nyamod <nyamsurennyamod9@gmail.com>
2026-09-12 18:58:56 +0800

    SLO threshold: FAIL demonstrated (1000 VU, p95=4.48s, 4.52% errors)

--- scripts/slo-fail.js
+++ scripts/slo-fail.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 300,
+  vus: 1000,
   duration: '30s',
   thresholds: {
     http_req_duration: ['p(95)<408'],
